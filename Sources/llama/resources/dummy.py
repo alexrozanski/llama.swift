03aa4392b6bcf59c7621e61898e0730eb8954beb
Alex Rozanski <alex@rozanski.me>
2023-04-08 23:23:47 +0100

    tweak dummy script

--- Sources/llama/resources/dummy.py
+++ Sources/llama/resources/dummy.py
@@ -6,10 +6,9 @@ import torch
 from sentencepiece import SentencePieceProcessor
 
 print("Starting")
-time.sleep(2)
+time.sleep(1)
 print(f'input: {sys.argv[1]}')
-time.sleep(2)
-print(f'output: {sys.argv[2]}')
-time.sleep(2)
-print("3")
-time.sleep(2)
+print(f'writing file...')
+fname_out = f"{sys.argv[1]}/ggml-model-1.bin"
+with open(fname_out, "wb") as fout:
+  fout.write(b" ")

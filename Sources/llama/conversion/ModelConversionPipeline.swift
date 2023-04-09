e2551369be3b30e1df3c13a046644301c583c7d2
Alex Rozanski <alex@rozanski.me>
2023-04-09 14:54:45 +0100

    make steps public

--- Sources/llama/conversion/ModelConversionPipeline.swift
+++ Sources/llama/conversion/ModelConversionPipeline.swift
@@ -23,7 +23,7 @@ public class ModelConversionPipeline<StepType, InputType, ResultType> {
     }
   }
 
-  var steps: [ModelConversionStep<StepType, Any, Any>] {
+  public var steps: [ModelConversionStep<StepType, Any, Any>] {
     return pipeline.steps
   }
 

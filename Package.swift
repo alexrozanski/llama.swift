57da9af638b95a0ec56154b75fd59ca9f1f78315
Alex Rozanski <alex@rozanski.me>
2023-04-10 04:14:44 +0100

    use v0.3 of Coquille

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     .library(name: "llama", targets: ["llama"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/alexrozanski/Coquille.git", from: "0.2.0")
+    .package(url: "https://github.com/alexrozanski/Coquille.git", branch: "v0.3")
   ],
   targets: [
     .target(
